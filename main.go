80b5f6e2528619cc4761b7d3c68256d0ac1f5ffc
langhuihui <178529795@qq.com>
2023-12-13 15:20:35 +0800

    feat: add desc to config

--- main.go
+++ main.go
@@ -15,72 +15,82 @@ import (
 
 type HookData struct {
 	*Stream `json:"stream"`
-	Extra   map[string]interface{} `json:"extra"`
-	Event   string                 `json:"event"` //事件名称
-	Time    int64                  `json:"time"`  //调用时间
+	Extra   map[string]any `json:"extra"`
+	Event   string         `json:"event"` //事件名称
+	Time    int64          `json:"time"`  //调用时间
 }
 
 type HookAddr struct {
-	URL    string
-	Header map[string]string
-	Method string
+	URL    string            `desc:"上报远程地址"`
+	Header map[string]string `desc:"附加 HTTP 头"`
+	Method string            `desc:"HTTP请求方法，默认为POST"`
 }
 
 type HookConfig struct {
-	KeepAlive   int
-	RetryTimes  int
-	BaseURL     string
-	Header      map[string]string
-	URLList     map[string]string
-	RequestList map[string]*HookAddr
-	Extra       map[string]interface{}
+	KeepAlive   time.Duration     `desc:"保活间隔，0则不发送保活事件"`
+	RetryTimes  int               `default:"3" desc:"重试次数"`
+	BaseURL     string            `desc:"URL前缀，如果URL不是http开头，则会自动加上前缀"`
+	Header      map[string]string `desc:"附加 HTTP 头" key:"名称" value:"值"`
+	RequestList map[string]any    `desc:"请求列表是需要单独配置 Method 和 header 时用到的，key为事件名称（*代表全部），value为请求信息,可以是字符串也可以是键值对（可以包含 url、header、method"`
+	Extra       map[string]any    `desc:"附加的额外参数"`
+	requestList map[string]*HookAddr
 }
 
-var plugin = InstallPlugin(&HookConfig{
-	RetryTimes: 3,
-})
+var HookPlugin = InstallPlugin(&HookConfig{})
 
 func (h *HookConfig) OnEvent(event any) {
 	switch v := event.(type) {
 	case FirstConfig:
-		if h.RequestList == nil {
-			h.RequestList = make(map[string]*HookAddr)
-		}
-		if h.URLList == nil {
-			h.URLList = make(map[string]string)
-		}
-		if h.BaseURL != "" {
-			for k, u := range h.URLList {
-				if !strings.HasSuffix(u, "http") {
-					h.URLList[k] = h.BaseURL + u
+		h.requestList = make(map[string]*HookAddr)
+		for k, v := range h.RequestList {
+			switch u := v.(type) {
+			case string:
+				if !strings.HasSuffix(u, "http") && h.BaseURL != "" {
+					u = h.BaseURL + u
 				}
-			}
-			for _, u := range h.RequestList {
-				if !strings.HasSuffix(u.URL, "http") {
-					u.URL = h.BaseURL + u.URL
+				h.requestList[k] = &HookAddr{URL: u, Method: "POST", Header: h.Header}
+			case map[string]string:
+				var addr = HookAddr{
+					Method: u["method"],
+					Header: h.Header,
+					URL:    u["url"],
 				}
-				if u.Header != nil {
-					if h.Header != nil {
-						for k, v := range h.Header {
-							u.Header[k] = v
+				if !strings.HasSuffix(addr.URL, "http") && h.BaseURL != "" {
+					addr.URL = h.BaseURL + addr.URL
+				}
+				h.requestList[k] = &addr
+			case map[string]any:
+				var addr HookAddr
+				for k, v := range u {
+					switch k {
+					case "url":
+						addr.URL = v.(string)
+						if !strings.HasSuffix(addr.URL, "http") && h.BaseURL != "" {
+							addr.URL = h.BaseURL + addr.URL
 						}
+					case "method":
+						addr.Method = v.(string)
+					case "header":
+						addr.Header = v.(map[string]string)
 					}
-				} else {
-					u.Header = h.Header
 				}
+				if addr.Header == nil {
+					addr.Header = h.Header
+				} else {
+					for k, v := range h.Header {
+						addr.Header[k] = v
+					}
+				}
+				h.requestList[k] = &addr
 			}
 		}
-		for k, u := range h.URLList {
-			if _, ok := h.RequestList[k]; !ok {
-				h.RequestList[k] = &HookAddr{URL: u, Method: "POST", Header: h.Header}
-			}
-		}
+
 		h.request(&HookData{Event: "startup"})
 		var heartreq []*HookAddr
-		if v, ok := h.RequestList["keepalive"]; ok {
+		if v, ok := h.requestList["keepalive"]; ok {
 			heartreq = append(heartreq, v)
 		}
-		if v, ok := h.RequestList["*"]; ok {
+		if v, ok := h.requestList["*"]; ok {
 			heartreq = append(heartreq, v)
 		}
 		if h.KeepAlive > 0 && len(heartreq) > 0 {
@@ -91,7 +101,7 @@ func (h *HookConfig) OnEvent(event any) {
 							Event: "keepalive",
 						})
 					}
-					time.Sleep(time.Second * time.Duration(h.KeepAlive))
+					time.Sleep(h.KeepAlive)
 				}
 			}(heartreq...)
 		}
@@ -100,16 +110,16 @@ func (h *HookConfig) OnEvent(event any) {
 	case SEclose:
 		h.request(&HookData{Event: "streamClose", Stream: v.Target})
 	case ISubscriber:
-		h.request(&HookData{Event: "subscribe", Stream: v.GetSubscriber().Stream, Extra: map[string]interface{}{"subscriber": v}})
+		h.request(&HookData{Event: "subscribe", Stream: v.GetSubscriber().Stream, Extra: map[string]any{"subscriber": v}})
 	case UnsubscribeEvent:
-		h.request(&HookData{Event: "unsubscribe", Stream: v.Target.GetSubscriber().Stream, Extra: map[string]interface{}{"subscriber": v.Target}})
+		h.request(&HookData{Event: "unsubscribe", Stream: v.Target.GetSubscriber().Stream, Extra: map[string]any{"subscriber": v.Target}})
 	}
 }
 func (h *HookConfig) request(hookData *HookData) {
-	if req, ok := h.RequestList[hookData.Event]; ok {
+	if req, ok := h.requestList[hookData.Event]; ok {
 		go h.doRequest(req, hookData)
 	}
-	if req, ok := h.RequestList["*"]; ok {
+	if req, ok := h.requestList["*"]; ok {
 		go h.doRequest(req, hookData)
 	}
 }
